3f60f5a022ba2b6fea3b091a46a2796d65f4b4bc
angela paco <janice.paco@gmail.com>
2024-08-05 01:37:10 +0800

    reroute + contact svg fix

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -14,14 +14,14 @@ const Contact = () => {
         <div className="github-wrapper">
           <p>My Github profile</p>
           <a href="https://github.com/angelapaco" target="_blank" className='contact-link' onMouseEnter={playBoop}>
-            <img src="src/assets/svg/github-mark.svg" alt="email svg icon"/>
+            <img src={import.meta.env.BASE_URL + "src/assets/svg/github-mark.svg"} alt="email svg icon"/>
             <p>angelapaco</p>
           </a>
         </div>
         <div className="email-wrapper">
           <p>You can send me an email here!</p>
           <a href="mailto:janice.paco@gmail.com" className='contact-link' onMouseEnter={playBoop}>
-            <img src="src/assets/svg/email.svg" alt="email svg icon"/>
+            <img src={import.meta.env.BASE_URL + "src/assets/svg/email.svg"} alt="email svg icon"/>
             <p>janice.paco@gmail.com</p>
           </a>
         </div>
