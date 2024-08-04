8e0f89a4c67aebdef3c2ccbb193016602f7d7333
angela paco <janice.paco@gmail.com>
2024-08-05 02:40:16 +0800

    contact svg fix

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './styles/Contact.css'
 import useSound from 'use-sound';
 import boopLink from '/src/assets/audio/boopLink.mp3';
+import githubLogo from '/src/assets/svg/github-mark.svg';
+import emailLogo from '/src/assets/svg/email.svg';
 
 const Contact = () => {
   const boopLinkSrc = boopLink;
@@ -14,14 +16,14 @@ const Contact = () => {
         <div className="github-wrapper">
           <p>My Github profile</p>
           <a href="https://github.com/angelapaco" target="_blank" className='contact-link' onMouseEnter={playBoop}>
-            <img src={import.meta.env.BASE_URL + "src/assets/svg/github-mark.svg"} alt="email svg icon"/>
+            <img src={githubLogo} alt="github svg icon"/>
             <p>angelapaco</p>
           </a>
         </div>
         <div className="email-wrapper">
           <p>You can send me an email here!</p>
           <a href="mailto:janice.paco@gmail.com" className='contact-link' onMouseEnter={playBoop}>
-            <img src={import.meta.env.BASE_URL + "src/assets/svg/email.svg"} alt="email svg icon"/>
+            <img src={emailLogo} alt="email svg icon"/>
             <p>janice.paco@gmail.com</p>
           </a>
         </div>
